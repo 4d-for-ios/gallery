7c699b06897e0da8987f8b525e6ff8c382295193
Eric Marchand <59135882+mesopelagique@users.noreply.github.com>
2020-02-14 15:14:07 +0100

    Update Item.vue

--- website/src/components/Item.vue
+++ website/src/components/Item.vue
@@ -27,7 +27,7 @@
             <span class="v-align-baseline">{{item.version}}</span>
           </div>
           <div class="col-8"></div>
-          <div class="col-2 cursor-pointer" @click="download">
+          <div v-tooltip.top-center="Download" class="col-2 cursor-pointer" @click="download">
             <font-awesome-icon :icon="['fas', 'cloud-download-alt']" />
             <span class="v-align-middle">{{item.download_count}}</span>
           </div>
@@ -35,7 +35,7 @@
             <font-awesome-icon icon="star" />
             <span class="v-align-middle">{{item.stargazers_count}}</span>
           </div>
-          <div class="col-2 cursor-pointer" @click="openURL">
+          <div v-tooltip.top-center="Open project page" class="col-2 cursor-pointer " @click="openURL" >
             <font-awesome-icon :icon="['fab', 'github']" />
           </div>
         </div>
@@ -93,5 +93,98 @@ export default {
 <style>
 .image { 
 
+}
+.tooltip {
+  display: block !important;
+  z-index: 10000;
+
+  .tooltip-inner {
+    background: black;
+    color: white;
+    border-radius: 16px;
+    padding: 5px 10px 4px;
+  }
+
+  .tooltip-arrow {
+    width: 0;
+    height: 0;
+    border-style: solid;
+    position: absolute;
+    margin: 5px;
+    border-color: black;
+    z-index: 1;
+  }
+
+  &[x-placement^="top"] {
+    margin-bottom: 5px;
+
+    .tooltip-arrow {
+      border-width: 5px 5px 0 5px;
+      border-left-color: transparent !important;
+      border-right-color: transparent !important;
+      border-bottom-color: transparent !important;
+      bottom: -5px;
+      left: calc(50% - 5px);
+      margin-top: 0;
+      margin-bottom: 0;
+    }
+  }
+
+  &[x-placement^="bottom"] {
+    margin-top: 5px;
+
+    .tooltip-arrow {
+      border-width: 0 5px 5px 5px;
+      border-left-color: transparent !important;
+      border-right-color: transparent !important;
+      border-top-color: transparent !important;
+      top: -5px;
+      left: calc(50% - 5px);
+      margin-top: 0;
+      margin-bottom: 0;
+    }
+  }
+
+  &[x-placement^="right"] {
+    margin-left: 5px;
+
+    .tooltip-arrow {
+      border-width: 5px 5px 5px 0;
+      border-left-color: transparent !important;
+      border-top-color: transparent !important;
+      border-bottom-color: transparent !important;
+      left: -5px;
+      top: calc(50% - 5px);
+      margin-left: 0;
+      margin-right: 0;
+    }
+  }
+
+  &[x-placement^="left"] {
+    margin-right: 5px;
+
+    .tooltip-arrow {
+      border-width: 5px 0 5px 5px;
+      border-top-color: transparent !important;
+      border-right-color: transparent !important;
+      border-bottom-color: transparent !important;
+      right: -5px;
+      top: calc(50% - 5px);
+      margin-left: 0;
+      margin-right: 0;
+    }
+  }
+
+  &[aria-hidden='true'] {
+    visibility: hidden;
+    opacity: 0;
+    transition: opacity .15s, visibility .15s;
+  }
+
+  &[aria-hidden='false'] {
+    visibility: visible;
+    opacity: 1;
+    transition: opacity .15s;
+  }
 }
 </style>
