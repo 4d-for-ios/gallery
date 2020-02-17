c6c24916856d61b0eb130bbda29cf951b496e55c
runner <runner@Mac-1638.local>
2020-02-17 16:44:39 +0000

    Deploy website version based on d6e2704c643cc12d124ae67d6451d9a2a36155ff

--- src/components/Item.vue
+++ src/components/Item.vue
@@ -2,11 +2,11 @@
   <div class="col-sm-6 mb-3 mt-3 d-flex flex-column">
     <div class="flex-column bg-white rounded-1 box-shadow border text-inherit no-underline">
       <div class="d-flex flex-row">
-        <div class="col-sm-3 v-align-middle pt-1 pl-1">
+        <div class="col-3 col-sm-3 v-align-middle pt-1 pl-1">
           <img 
             v-if="item.has_image"
             v-bind:src="item.image_url"
-            v-img:items='{title: item.name, opened: openG, changed: changed }'
+            v-img:items='{title: item.name, cursor: "zoom-in", opened: openG, changed: changed }'
             class = "image"
             :data-download_url="item.download_url"
           />
@@ -16,7 +16,7 @@
             :src="item.image_url"
           />
         </div>
-        <div class="col-sm-9 text-left cursor-pointer" @click="mainAction">
+        <div class="col-9 col-sm-9 text-left cursor-pointer menu-item" @click="mainAction">
           <h3 class="h4 px-3 pt-3">{{item.name}}</h3>
           <p class="text-gray lh-condensed px-3 pb-3">{{item.description}}</p>
         </div>
@@ -26,19 +26,20 @@
         style="margin-top: auto; border-bottom-left-radius: 0; border-bottom-right-radius: 0;"
       >
         <div class="d-flex flex-row ml-1">
-          <div v-if="picker === null" class="col-sm-2 cursor-pointer tooltipped tooltipped-s" :aria-label='item.download_count+" installations"' @click="download">
+          <div v-if="picker == null" class="col-3 col-sm-3 cursor-pointer tooltipped tooltipped-s" :aria-label='item.download_count+" installations"' @click="download">
             <font-awesome-icon :icon="['fas', 'cloud-download-alt']" />
             <span class="v-align-baseline">{{item.download_count}}</span>
           </div>
-          <div v-if="picker === null" class="col-sm-2 cursor-pointer tooltipped tooltipped-s" :aria-label='item.stargazers_count+" stars"' @click="stargazer">
+          <div v-if="picker == null" class="col-2 col-sm-2 cursor-pointer tooltipped tooltipped-s" :aria-label='item.stargazers_count+" stars"' @click="stargazer">
             <font-awesome-icon icon="star" />
             <span class="v-align-baseline">{{item.stargazers_count}}</span>
           </div>
-          <div class="col-sm-8"></div>
-          <div class="col-sm-2 cursor-pointer tooltipped tooltipped-s" aria-label="Open on github" @click="openURL" >
+          <div v-else class="col-5 col-sm-5"></div> 
+          <div class="col-4 col-sm-4"></div>
+          <div class="col-1 col-sm-1 cursor-pointer tooltipped tooltipped-s" aria-label="Open on github" @click="openURL" >
             <font-awesome-icon :icon="['fab', 'github']" />
           </div>
-          <div class="col-sm-2 tooltipped tooltipped-s" aria-label="Latest version">
+          <div class="col-2 col-sm-2 tooltipped tooltipped-s" aria-label="Latest version">
             <span class="v-align-baseline">{{item.version}}</span>
           </div>
         </div>
@@ -138,5 +139,8 @@ export default {
 <style scoped>
 .image {
   max-width: 50px;
+}
+.cursor-pointer {
+  cursor: pointer;
 }
 </style>
