6e691a5b44866d00a1d0be1b19c25a1a02620459
runner <runner@Mac-1288.local>
2020-03-02 15:27:08 +0000

    Deploy website version based on 610b9d689027c693bf2d8b73c9d2201cf54af360

--- src/components/GalleryItem.vue
+++ src/components/GalleryItem.vue
@@ -1,36 +1,34 @@
 <template>
-  <div class="col-6 col-sm-6 mb-3 mt-3 d-flex flex-column cursor-pointer">
+  <div class="col-6 col-sm-6 mb-3 mt-3 d-flex flex-column">
     <div class="item-card">
       <div
         class="d-flex flex-column flex-1 rounded-1 border border-dashed border-gray-dark link-gray no-underline"
       >
-        <h3 class="h4 px-3 pt-3 text-left" @click="share">
+        <h3 class="h4 px-3 pt-3 text-left cursor-pointer" @click="share">
           <font-awesome-icon :icon="['fas', 'plus']" />
         Share your own
         </h3>
         <p
-          class="lh-condensed px-3 pb-3 text-left"
+          class="lh-condensed px-3 pb-3 text-left cursor-pointer"
           @click="share"
         >Is your project is ready for prime time? Submit it.</p>
-
         <div
-          class="text-gray-light pl-3 pr-2 py-2 bg-gray-light border-top"
+          class="pl-3 pr-2 py-2 bg-gray-light border-top"
           style="margin-top: auto; border-bottom-left-radius: 0; border-bottom-right-radius: 0;"
         >
           <div class="d-flex flex-row">
-            
             <div class="col-4 col-sm4">
-              <a href="https://github.com/4d-for-ios/gallery/blob/master/.github/CONTRIBUTING.md">
+               <div @click="openContributing" class="menu-item cursor-pointer">
                 <font-awesome-icon icon="question-circle" />
                 <span class="v-align-baseline">Contributing Guide</span>  
-              </a>
+              </div>
             </div>
-            <div class="col-5 col-sm5">
+            <div class="col-4 col-sm4">
             </div>
-            <div class="col-3 col-sm3">
-              <div  @click="fullGallery">
+            <div class="col-4 col-sm4">
+              <div @click="fullGallery" class="menu-item cursor-pointer">
                 <font-awesome-icon icon="external-link-alt" />
-                <span class="v-align-baseline">Gallery on Web Browser</span>  
+                <span class="v-align-baseline">Open in Browser</span>  
               </div>
             </div>
           </div>
@@ -49,6 +47,11 @@ export default {
     };
   },
   methods: {
+    openContributing() {
+      window.open(
+        "https://github.com/4d-for-ios/gallery/blob/master/.github/CONTRIBUTING.md"
+      );
+    },
     fullGallery() {
       var location = window.location
       var rootURL = location.protocol+"//"+location.host+location.pathname
@@ -65,3 +68,8 @@ export default {
   }
 };
 </script>
+<style scoped>
+.cursor-pointer {
+  cursor: pointer;
+}
+</style>
