7270380b2021f444f78563cc4588ce4468e3ab6d
Eric Marchand <59135882+mesopelagique@users.noreply.github.com>
2020-07-15 15:34:41 +0200

    Update push.sh

--- .github/push.sh
+++ .github/push.sh
@@ -5,4 +5,5 @@ echo "Push to branch $BRANCH"
 
 remote_repo="https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
 
+echo "actor=${GITHUB_ACTOR}, branch=${BRANCH}, repo=${GITHUB_REPOSITORY}"
 git push "${remote_repo}" HEAD:${BRANCH}
